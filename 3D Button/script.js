ade1e7bdcaf3b25a3462c59cce6d78d3d9356679
janabdullahi <abdullahi@netlinks.af>
2023-06-13 10:52:49 +0430

    items will remove when the mouse is out of button

--- 3D Button/script.js
+++ 3D Button/script.js
@@ -3,4 +3,8 @@ const menu = document.querySelector(".menu");
 
 centerItem.addEventListener("mouseover", () =>{
     menu.classList.add("change");
-})
+});
+
+menu.addEventListener("mouseout", () =>{
+    menu.classList.remove("change");
+});
